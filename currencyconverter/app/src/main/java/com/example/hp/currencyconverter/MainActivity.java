6ea9a7d4995127da772b03abf45c334de24c8f08
vinamra bathwal <vinamrabathwal99@gmail.com>
2018-08-07 22:59:31 +0530

    App Completed

---
+++ currencyconverter/app/src/main/java/com/example/hp/currencyconverter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hp.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertFunction (View view) {
+
+        EditText Edittext1 = findViewById(R.id.Edittext1);
+        +
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
